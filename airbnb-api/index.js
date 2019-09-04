bc76e8a2e47a544befdc69794348435242901ec7
Eltech-git <emilianolombarditech@gmail.com>
2019-09-04 16:07:06 +0700

    080202

--- airbnb-api/index.js
+++ airbnb-api/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 let app = express();
 
+app.get("/prova", (req, res) => {
+  res.send("Welcome to Airbnb Api");
+});
+
 app.listen(4000, () => {
   console.log("The server you request is ready and running on port 4000");
 });
